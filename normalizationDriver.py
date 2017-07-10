f4a471de93d242cb6699a619282fbf5728b8e761
Aditya Gune <adi.vtca@gmail.com>
2017-07-10 11:28:12 -0700

    Added handlers for dependency checking

--- normalizationDriver.py
+++ normalizationDriver.py
@@ -1,12 +1,13 @@
 """
-NORMALIZEDATA.PY
+NORMALIZATIONDRIVER.PY
 
 Driver program that performs data normalization for Mill Data tables.
 Some tables will require further normalization.
 
 INPUT: CSV of your table, taken from Access
 OUTPUT: Normalized CSV to be imported to SQL Server
-DEPENDENCIES: csvreader.py
+DEPENDENCIES: csvreader.py, sourceNametoFK.py, MillIDConversion.py,
+                MillIDConversion.csv, sourcestbl.csv
 
 INPUT PARAMETERS:
 
@@ -15,14 +16,32 @@ INPUT PARAMETERS:
 
     filename: Filename of your input file. Enter the extension.
 """
+
+#MAKE YOUR CHANGES HERE
+sourceCol = 5
+filename = 'siccodes.csv'
+#########################
+
+import sys, os
+
+#handle dependencies
+if (not os.path.isfile('MillIDConversion.csv') or
+    not os.path.isfile('sourcestbl.csv') or
+    not os.path.isfile('sourceNametoFK.py') or
+    not os.path.isfile('MillIDConversion.py')):
+    print("ERROR: Missing dependencies. Please make sure you have the following \nfiles in your directory:")
+    print('\t -csvreader.py\n\t -sourceNametoFK.py\n\t -MillIDConversion.py\n\t -MillIDConversion.csv\n\t -sourcestbl.csv')
+    sys.exit()
+
+if not os.path.isfile(filename):
+    print("ERROR: No such file as", "'" + filename + "'")
+    sys.exit()
+
+#import dependencies
 from sourceNametoFK import *
 from MillIDConversion import *
 from csvreader import *
-import csv
-
-sourceCol = 5
-filename = 'SludgeData.csv'
 
 newFile = convertMillIDs(filename)
-newFile = convertSources(millIDConverted, sourceCol)
-print("\n **** Your target file is: ", newFile, " ****")
+#newFile = convertSources(millIDConverted, sourceCol)
+#print("\n **** Your target file is: ", newFile, " ****")
