8e354b506398dbce2592c2ef652e697c2b0d2db9
Aditya Gune <adi.vtca@gmail.com>
2017-07-26 16:14:49 -0700

    handling blanks

--- millAFPA.py
+++ millAFPA.py
@@ -4,15 +4,20 @@ f = readCSV('millafpa.csv')
 t = open('millafpa.sql', 'w')
 updatestr = "UPDATE dbo.MillInformation"
 gostr = "GO\n"
-
+total = 0
+ctr = 0
 for i in f:
-
+    total +=1 
+    if i[0] == '':
+        continue
     valstr = " SET MillInformation.AFPACode = " + i[0]
     wherestr = " WHERE MillInformation.MillID = " + i[1] + "\n"
     updateSql = updatestr + valstr + wherestr
     t.write(updateSql)
-
+    ctr += 1
 t.write(gostr)
 t.close()
 
-
+print("=====================================")
+print(ctr, "out of", total, "added.")
+print("=====================================")
