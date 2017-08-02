4811b0e148a2a5d43e3aee07410e7c45466e0955
Aditya Gune <adi.vtca@gmail.com>
2017-08-02 11:02:49 -0700

    final version

--- insertWoodSQL.py
+++ insertWoodSQL.py
@@ -5,8 +5,8 @@ t = open('insertwood.sql', 'w')
 del p[0]
 trystr = "BEGIN TRY \n"
 insertstr = "INSERT INTO dbo.EHSSURVEY_WoodThickness ([WoodID],[Amount],[Thickness],[CF],[Units])\n VALUES"
-sql = trystr + insertstr
 
+t.write(trystr)
 for i in p:
     empty = 0
     wid = i[0]
@@ -16,29 +16,34 @@ for i in p:
     units = i[4]
     
     if thk == '':
-        empty = 1
+        empty += 1
         thk = "NULL"
-    if amt == '':
-        empty = 1
-        amt == "NULL"
-    if cf == '':
-        empty = 1
-        cf == "NULL"
-    if units == '':
-        empty = 1
-        units == "NULL"
 
-    if i == p[-1]:
-        valstr = " (" + wid + "," + amt + "," + thk + "," + cf + ",'" + units + "')\n"
-    valstr = " (" + wid + "," + amt + "," + thk + "," + cf + ",'" + units + "'),\n"
-    if empty == 1:
+    if amt == '':
+        empty += 1
+        amt = "NULL"
+        print("Amount empty, changed to ", repr(amt))
+        
+    if cf == '':
+        empty += 1
+        cf = "NULL"
+        print("CF empty, changed to ", repr(cf))
+    if units == '':
+        empty += 1
+        units = "NULL"
+        print("Units empty, changed to ", repr(units))
+
+    
+    valstr = " (" + wid + "," + amt + "," + thk + "," + cf + ",'" + units + "')\n"
+    sql = insertstr
+    if empty > 1:
         print(repr(i))
         print(valstr)
         print()
         
 
-    sql +=  valstr
-
+    sql += valstr  
+    t.write(sql)
 catchsql = """end try
 begin catch
     --returns the complete original error message as a result set
@@ -54,8 +59,6 @@ begin catch
     DECLARE @ErrorMessage nvarchar(400), @ErrorNumber int, @ErrorSeverity int, @ErrorState int, @ErrorLine int
     SELECT @ErrorMessage = N'Error %d, Line %d, Message: '+ERROR_MESSAGE(),@ErrorNumber = ERROR_NUMBER(),@ErrorSeverity = ERROR_SEVERITY(),@ErrorState = ERROR_STATE(),@ErrorLine = ERROR_LINE()
     RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState, @ErrorNumber,@ErrorLine)
-end catch"""
-
-sql += catchsql
-t.write(sql)
+end catch\n"""
+t.write(catchsql)
 t.close()
